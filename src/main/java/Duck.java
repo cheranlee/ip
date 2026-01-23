0a3cd5749cd388afc976a66d06898a002c01224a
cheranlee <cheranlee03@gmail.com>
2026-01-24 00:42:52 +0800

    Level-0: Rename, Greet, Exit
    Rename Duke.java to Duck.java
    
    Edit Skeleton Duke.java Code

---
+++ src/main/java/Duck.java
@@ -0,0 +1,8 @@
+public class Duck {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Quack! I'm Duck. \nWhat can I do for you?");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Quack Quack! Hope to see you again soon!");
+    }
+}
