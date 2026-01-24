4ee71c04e7d039b5dcab9ca78db7e449c71edc60
cheranlee <cheranlee03@gmail.com>
2026-01-24 12:07:48 +0800

    Level-1: Echo
    Echo user commands
    
    Exits when user types bye

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Duck {
     public static void main(String[] args) {
         System.out.println("-------------------------------------------------------------");
         System.out.println("Quack! I'm Duck. \nWhat can I do for you?");
         System.out.println("-------------------------------------------------------------");
-        System.out.println("Quack Quack! Hope to see you again soon!");
+
+        Scanner new_object = new Scanner(System.in);
+        String command = new_object.nextLine();
+        while (!command.contains("bye")){
+            System.out.println("\t-------------------------------------------------------------");
+            System.out.println('\t'+ command);
+            System.out.println("\t-------------------------------------------------------------");
+            command = new_object.nextLine();
+        }
+        System.out.println("\t-------------------------------------------------------------");
+        System.out.println("\tBye Quack! Hope to see you again soon!");
+        System.out.println("\t-------------------------------------------------------------");
     }
 }
